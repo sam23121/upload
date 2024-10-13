259cd80e646b4655169ea8d758dbc0ec21527d70
sam23121 <smlalene@gmail.com>
2024-10-13 16:29:02 +0300

    fix: working on the build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
+    outDir: 'dist',
+    sourcemap: true,
   },
 })
