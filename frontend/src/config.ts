6685c6e51c839ff4c43b95c264d16c85b872d3a0
sam23121 <smlalene@gmail.com>
2024-10-13 20:07:55 +0300

    feat: remove last slash

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,4 @@
-export const API_BASE_URL = 'https://ru7n6uh4i72w5qpsplhkqsurfq0fvndk.lambda-url.us-east-1.on.aws/';
+export const API_BASE_URL = 'https://ru7n6uh4i72w5qpsplhkqsurfq0fvndk.lambda-url.us-east-1.on.aws';
+// export const API_BASE_URL = 'http://localhost:8002';
+
 
